81bc35ee0f876f5322c0ac8c813d86511f5e534b
archuser-q <availableemailaddress123@tutamail.com>
2025-09-26 21:27:49 +0700

    update

--- app/setting.tsx
+++ app/setting.tsx
@@ -1,17 +1,45 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, Switch, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const Radar = () => {
-    return(
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-            <Text>Radar page</Text>
+  const [darkMode, setDarkMode] = useState(false);
+  const [notifications, setNotifications] = useState(true);
+  const [locationAccess, setLocationAccess] = useState(false);
+
+  return (
+    <SafeAreaProvider>
+      <ScrollView>
+        <View className="flex-1 pt-20 px-5 space-y-6">
+          <Text className="text-4xl font-bold mb-4">Settings</Text>
+
+          <View className="flex-row items-center justify-between">
+            <Text className="text-xl">Dark/light mode</Text>
+            <Switch
+              value={darkMode}
+              onValueChange={setDarkMode}
+            />
+          </View>
+
+          <View className="flex-row items-center justify-between">
+            <Text className="text-xl">Notifications</Text>
+            <Switch
+              value={notifications}
+              onValueChange={setNotifications}
+            />
+          </View>
+
+          <View className="flex-row items-center justify-between">
+            <Text className="text-xl">Location access</Text>
+            <Switch
+              value={locationAccess}
+              onValueChange={setLocationAccess}
+            />
+          </View>
         </View>
-    );
-}
+      </ScrollView>
+    </SafeAreaProvider>
+  );
+};
 
 export default Radar;
